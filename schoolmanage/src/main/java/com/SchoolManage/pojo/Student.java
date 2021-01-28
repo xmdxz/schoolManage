aaa8ce60ecf9e99cf6b1628f7691418f3d567fc1
RainGoal <88344822@qq.com>
2021-01-28 08:58:59 +0800

    添加学生实体

---
+++ schoolmanage/src/main/java/com/SchoolManage/pojo/Student.java
@@ -0,0 +1,40 @@
+package com.SchoolManage.pojo;
+
+import lombok.*;
+
+/**
+ * @Author RainGoal
+ * @Date 2021/1/28 8:41
+ * @Description TODO
+ * @Version 1.0
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Student {
+    private String id;
+    private String name;
+    private String sex;
+    private String comy;
+    private String clno;
+    private String major;
+    private String present_class;
+    private String phone;
+    private String bedroom_lou;
+    private String birth;
+    private String nativeplace;
+    private String address;
+    private String father_phone;
+    private String mother_phone;
+    private String photo;
+    private String original_class;
+    private String original_post;
+    private String present_post;
+    private String nation;
+    private String religion;
+    private String bedroom_hao;
+    private String idcard;
+    private String bank;
+}
