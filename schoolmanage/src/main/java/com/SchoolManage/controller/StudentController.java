ab224c47d7c68da79098485d256d57dbccd2b123
yuze <649946725@qq.com>
2021-02-03 22:41:56 +0800

    学号搜索和姓名搜索的完善

--- schoolmanage/src/main/java/com/SchoolManage/controller/StudentController.java
+++ schoolmanage/src/main/java/com/SchoolManage/controller/StudentController.java
@@ -79,13 +79,16 @@ public class StudentController {
     @RequestMapping("findbyname")
     @ResponseBody
     public List<Student> findByName(String name,int page,int num) {
+        System.out.println("name"+name);
+        System.out.println("page"+page);
+        System.out.println("num"+num);
         List<Student> byName = studentService.findByName(name,page,num);
         return byName;
     }
     @RequestMapping("findByNameCount")
     @ResponseBody
     public String findByNameCount(String name) {
-        System.out.println(name);
+
         int i = studentService.findByNameCount(name);
         return Integer.toString(i);
     }
