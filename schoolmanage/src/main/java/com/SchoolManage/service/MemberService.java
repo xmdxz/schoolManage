8b32370894a268491bafac1c6a9855751b71b223
RainGoal <88344822@qq.com>
2021-02-05 22:51:06 +0800

    test

---
+++ schoolmanage/src/main/java/com/SchoolManage/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.SchoolManage.service;
+
+/**
+ * @Author RainGoal
+ * @Date 2021/2/5 21:55
+ * @Description TODO
+ * @Version 1.0
+ */
+
+public interface MemberService {
+
+
+}
---
+++ schoolmanage/src/main/java/com/SchoolManage/service/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package com.SchoolManage.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author RainGoal
+ * @Date 2021/2/5 21:55
+ * @Description TODO
+ * @Version 1.0
+ */
+@Service
+public class MemberServiceImpl implements MemberService {
+
+
+}
