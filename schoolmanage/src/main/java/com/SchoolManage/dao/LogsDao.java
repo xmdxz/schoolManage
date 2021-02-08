2d69b8939df4ee8d7aa7fdfd95b9c05879c91ec6
xpdxz <1059853272@qq.com>
2021-02-08 18:14:59 +0800

    logs的dao

---
+++ schoolmanage/src/main/java/com/SchoolManage/dao/LogsDao.java
@@ -0,0 +1,60 @@
+package com.SchoolManage.dao;
+
+import com.SchoolManage.pojo.Logs;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface LogsDao {
+
+    /**
+     * 插入
+     *
+     * @param logs
+     * @return
+     */
+    int insertData(@Param(value = "logs") Logs logs);
+
+    /**
+     * 批量
+     *
+     * @param list
+     * @return
+     */
+    int insertDatas(@Param(value = "list") List<Logs> list);
+
+    /**
+     * 删除
+     *
+     * @param id
+     * @return
+     */
+    int deleteData(@Param(value = "id") Integer id);
+
+    /**
+     * 查找
+     *
+     * @return
+     */
+    List<Logs> findAll();
+
+    /**
+     * 查找数量
+     *
+     * @return
+     */
+    int findAllNum();
+
+    /**
+     * 更新记录
+     *
+     * @param logs
+     * @return
+     */
+    int updateData(Logs logs);
+
+}
