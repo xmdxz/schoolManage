69dcc303b1b424cd3dd4bcf9cc8f26c84692be11
RainGoal <88344822@qq.com>
2021-02-17 21:05:36 +0800

    修复报错

--- schoolmanage/src/main/java/com/SchoolManage/service/ActivityService.java
+++ schoolmanage/src/main/java/com/SchoolManage/service/ActivityService.java
@@ -13,13 +13,19 @@ import java.util.List;
  */
 
 public interface ActivityService {
-
     /**
      * 查找全部
      *
      * @return
      */
-    List<Activity> findAll();
+    List<Activity> findAll(Integer Page, Integer num);
+
+    /**
+     * 查询全部数量
+     *
+     * @return
+     */
+    int findAllCount();
 
     /**
      * 根据学生查询
@@ -27,7 +33,23 @@ public interface ActivityService {
      * @param student
      * @return
      */
-    List<Activity> findByStudent(String student);
+    List<Activity> findByStudentPage(String student, Integer Page, Integer num);
+
+    /**
+     * 根据学生查询，无分页
+     *
+     * @param student
+     * @return
+     */
+    List<Activity> findByStudentNoPage(String student);
+
+    /**
+     * 根据学生查询数量
+     *
+     * @param student
+     * @return
+     */
+    int findByStudentCount(String student);
 
     /**
      * 根据活动查询
@@ -35,15 +57,66 @@ public interface ActivityService {
      * @param active
      * @return
      */
-    List<Activity> findByActive(String active);
+    List<Activity> findByActive(String active, Integer Page, Integer num);
 
     /**
-     * 根据时间查询
+     * 根据活动查询数量
+     *
+     * @param active
+     * @return
+     */
+    int findByActiceCount(String active);
+
+    /**
+     * 根据年月日时间查询,活动的话，应该不会使用
      *
      * @param date
      * @return
      */
-    List<Activity> findByTime(Date date);
+    List<Activity> findByTime(Date date, Integer Page, Integer num);
+
+    /**
+     * 根据年月日查询数量
+     *
+     * @param date
+     * @return
+     */
+    int findByTimeCount(Date date);
+
+    /**
+     * 根据年月查询
+     *
+     * @param Page
+     * @param num
+     * @return
+     */
+    List<Activity> findByTimeYearAndMonth(Date time, Integer Page, Integer num);
+
+    /**
+     * 根据年月查询数量
+     *
+     * @param time
+     * @return
+     */
+    int findByTimeYearAndMonthCount(Date time);
+
+    /**
+     * 根据年查询
+     *
+     * @param time
+     * @param Page
+     * @param num
+     * @return
+     */
+    List<Activity> findByTimeYear(Date time, Integer Page, Integer num);
+
+    /**
+     * 根据年查询数量
+     *
+     * @param time
+     * @return
+     */
+    int findByTimeYearCount(Date time);
 
     /**
      * 插入单挑
