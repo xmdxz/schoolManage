5d4c353e9dd5b42c0be0de34b0025d2826fcb3dd
Y_Shub <383723959@qq.com>
2021-03-01 17:23:59 +0800

    增加多条后记录日志 上传

--- schoolmanage/src/main/java/com/SchoolManage/controller/MemberController.java
+++ schoolmanage/src/main/java/com/SchoolManage/controller/MemberController.java
@@ -195,7 +195,7 @@ public class MemberController {
                 i = memberService.BatchAddition(path);
                 dest.delete();
                 AdminUser a =(AdminUser) request.getSession().getAttribute("administer");
-                logService.insertNew("上窜","部门成员信息",a.getName(),"多条","部门成员表");
+                logService.insertNew("上传","部门成员信息",a.getName(),"多条","部门成员表");
                 return "上传成功了";
             } catch (Exception e) {
                 dest.delete();
